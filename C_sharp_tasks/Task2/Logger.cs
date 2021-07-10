d8d3c70df3e1733daa2176c3aa8b9a70e35a89ae
Alex <alex.tereshkov01@mail.ru>
2021-07-10 15:17:05 +0300

    Add Logger

---
+++ C_sharp_tasks/Task2/Logger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Task2
+{
+    public class Logger
+    {
+        private readonly StreamWriter _file;
+
+        public enum Severity
+        {
+            Trace,
+            Debug,
+            Information,
+            Warning,
+            Error,
+            Critical
+        }
+        
+        public Logger(string filename)
+        {
+            _file = new(filename);
+            _file.AutoFlush = true;
+        }
+        
+        public void Add(Severity severity, string data)
+        {
+            _file.WriteLine("[" +  DateTime.Now + "] [" + severity + "]: " + data);
+        }
+    }
+}
---
+++ C_sharp_tasks/Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger log = new("log.txt");
+            log.Add(Logger.Severity.Critical, "Something happened");
+            log.Add(Logger.Severity.Information, "Somewhere, IDK");
+            log.Add(Logger.Severity.Trace, "Oh my...");
+        }
+    }
+}
