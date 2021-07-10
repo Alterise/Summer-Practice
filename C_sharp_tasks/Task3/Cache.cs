1008a287441ac7bee45f6689471e5a43857dec68
Alex <alex.tereshkov01@mail.ru>
2021-07-10 20:40:02 +0300

    Cache DONE

---
+++ C_sharp_tasks/Task3/Cache.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task3
+{
+    public class Cache<T>
+    {
+        private readonly TimeSpan _lifetime;
+        private readonly int _maxSize;
+        private readonly Dictionary<string, Entry> _entries;
+        
+        private class Entry
+        {
+            public Entry(T value, DateTime creationTime)
+            {
+                _value = value;
+                _creationTime = creationTime;
+            }
+            
+            public readonly T _value;
+            public readonly DateTime _creationTime;
+        }
+        
+        public Cache(TimeSpan lifetime, int maxSize)
+        {
+            if (lifetime <= TimeSpan.Zero || maxSize <= 0)
+            {
+                throw new ArgumentException();
+            }
+            
+            _lifetime = lifetime;
+            _maxSize = maxSize;
+            _entries = new();
+        }
+        
+        public T Get(string key)
+        {
+            Update();
+            
+            if (!_entries.ContainsKey(key))
+            {
+                throw new KeyNotFoundException();
+            }
+            
+            return _entries[key]._value;
+        }
+
+        public void Save(string key, T value)
+        {
+            Update();
+
+            if (_entries.ContainsKey(key))
+            {
+                throw new ArgumentException();
+            }
+            
+            if (_maxSize == _entries.Count)
+            {
+                DateTime minTime = DateTime.MaxValue;
+                string keyToRemove = null;
+
+                foreach (var (currentKey, currentEntry) in _entries)
+                {
+                    if (currentEntry._creationTime < minTime)
+                    {
+                        minTime = currentEntry._creationTime;
+                        keyToRemove = currentKey;
+                    }
+                }
+
+                _entries.Remove(keyToRemove);
+
+            }
+            
+            _entries.Add(key, new Entry(value, DateTime.Now));
+        }
+
+        private void Update()
+        {
+            foreach (var (key, entry) in _entries)
+            {
+                if (DateTime.Now - entry._creationTime > _lifetime)
+                {
+                    _entries.Remove(key);
+                } 
+            }
+        }
+    }
+}
---
+++ C_sharp_tasks/Task3/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cache<int> cache = new Cache<int>(TimeSpan.FromSeconds(3), 3);
+            
+            cache.Save("3", 3);
+            cache.Save("2", 2);
+            cache.Save("1", 1);
+            cache.Save("4", 4);
+
+            try
+            {
+                cache.Get("3");
+            }
+            catch (KeyNotFoundException ex)
+            {
+                Console.WriteLine("There is no 3 in cache. It has been rewritten.");
+                Console.WriteLine();
+            }
+            
+            try
+            {
+                cache.Save("4", 4);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("4 already in cache.");
+            }
+
+            Console.WriteLine(cache.Get("1"));
+            Console.WriteLine(cache.Get("2"));
+            Console.WriteLine(cache.Get("4"));
+            Console.WriteLine();
+            
+            Console.WriteLine("Waiting for 3 seconds...");
+            Thread.Sleep(3000);
+            
+            try
+            {
+                cache.Get("1");
+            }
+            catch (KeyNotFoundException ex)
+            {
+                Console.WriteLine("There is no 1 in cache. It has been deleted.");
+            }
+            
+            try
+            {
+                cache.Get("2");
+            }
+            catch (KeyNotFoundException ex)
+            {
+                Console.WriteLine("There is no 2 in cache. It has been deleted.");
+            }
+            
+            try
+            {
+                cache.Get("4");
+            }
+            catch (KeyNotFoundException ex)
+            {
+                Console.WriteLine("There is no 4 in cache. It has been deleted.");
+            }
+        }
+    }
+}
