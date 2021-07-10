4aeb71d1563430474d1eea3020e8aa48fba6907e
Alex <alex.tereshkov01@mail.ru>
2021-07-10 20:41:18 +0300

    Add some stuff

---
+++ GaloisField/cmake-build-debug/CMakeFiles/GaloisField.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/GitHub/Summer-Practice/GaloisField/galois_field.cpp" "D:/GitHub/Summer-Practice/GaloisField/cmake-build-debug/CMakeFiles/GaloisField.dir/galois_field.cpp.obj"
+  "D:/GitHub/Summer-Practice/GaloisField/main.cpp" "D:/GitHub/Summer-Practice/GaloisField/cmake-build-debug/CMakeFiles/GaloisField.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ GaloisField/cmake-build-debug/CMakeFiles/GaloisField.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GaloisField.dir/galois_field.cpp.obj"
+  "CMakeFiles/GaloisField.dir/main.cpp.obj"
+  "GaloisField.exe"
+  "GaloisField.exe.manifest"
+  "GaloisField.pdb"
+  "libGaloisField.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GaloisField.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
